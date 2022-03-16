871e2d0fbe52cc6c007a3945a7a59adc344a3836
Jessy Yeh <jiejun.yeh@gmail.com>
2022-03-16 15:24:05 +0000

    Refactor

--- script.js
+++ script.js
@@ -1,62 +1,68 @@
 const gameBoard = document.querySelector(".game-board");
+const howToPlayBtn = document.querySelector(".how-to-play button");
+const howToPlayContent = document.querySelector(".howtoplay-content-container");
 const input = document.querySelector("input");
-const btn = document.querySelector(".submit-btn");
-const answersDiv = document.querySelector(".answers");
-const answerCorrect = document.querySelector(".correct");
-const answerWrong = document.querySelector(".wrong");
-const answerWrongSpan = document.querySelector(".wrong span");
-const invalidGuess = document.querySelector(".invalidword-alert");
-const howToPlay = document.querySelector(".how-to-play");
-const instructions = document.querySelector(".instructions-content-container");
+const invalidAlert = document.querySelector(".invalidword-alert");
+const answersDiv = document.querySelector(".answer-grid");
+const answerCorrect = document.querySelector(".success-container");
+const answerWrong = document.querySelector(".fail-container");
+const solutionSpan = document.querySelector(".fail span");
 
 const answers = ["", "", "", "", "", ""];
 let solution = "apple";
 let attemptNumber = 0;
 
-const solutionReqOptions = {
-  method: "GET",
-  url: "https://random-words5.p.rapidapi.com/getRandom",
-  params: { wordLength: "5" },
-  headers: {
-    "x-rapidapi-host": "random-words5.p.rapidapi.com",
-    "x-rapidapi-key": "235a134858mshf090c59729af53ap1e1eabjsn6b31f2f933a7",
-  },
-};
+// const solutionReqOptions = {
+//   method: "GET",
+//   url: "https://random-words5.p.rapidapi.com/getRandom",
+//   params: { wordLength: "5" },
+//   headers: {
+//     "x-rapidapi-host": "random-words5.p.rapidapi.com",
+//     "x-rapidapi-key": "235a134858mshf090c59729af53ap1e1eabjsn6b31f2f933a7",
+//   },
+// };
+
+// axios
+//   .request(solutionReqOptions)
+//   .then((response) => {
+//     console.log(response.data);
+//     // solution = response.data;
+//     // input.addEventListener("keyup", submitAnswer);
+//   })
+//   .catch((error) => {
+//     console.error(error);
+//   });
 
 input.addEventListener("keyup", submitAnswer);
 
-axios
-  .request(solutionReqOptions)
-  .then((response) => {
-    console.log(response.data);
-    // solution = response.data;
-    // btn.addEventListener("click", submitAnswer);
-    // input.addEventListener("keyup", submitAnswer);
-  })
-  .catch((error) => {
-    console.error(error);
-  });
-
 input.addEventListener("change", () => {
   const inputText = input.value;
+
   if (inputText.length !== 5) {
     input.classList.add("invalid-answer");
-    invalidGuess.classList.add("activate");
   } else {
     input.classList.remove("invalid-answer");
-    invalidGuess.classList.remove("activate");
   }
 });
 
-howToPlay.addEventListener("click", () => {
-  instructions.classList.add("display");
+howToPlayBtn.addEventListener("click", () => {
+  howToPlayContent.classList.add("display");
 });
-instructions.addEventListener("click", () => {
-  instructions.classList.remove("display");
+
+howToPlayContent.addEventListener("click", () => {
+  howToPlayContent.classList.remove("display");
+});
+
+answerCorrect.addEventListener("click", () => {
+  answerCorrect.classList.remove("display");
+});
+
+answerWrong.addEventListener("click", () => {
+  answerWrong.classList.remove("display");
 });
 
 function submitAnswer(e) {
-  if (e.code && e.code !== "Enter") return;
+  if (e.code !== "Enter") return;
 
   const inputText = input.value;
 
@@ -66,7 +72,6 @@ function submitAnswer(e) {
     updateHtml();
     checkAnswers();
     answerCorrect.classList.add("display");
-    // btn.removeEventListener("click", submitAnswer);
     input.removeEventListener("keyup", submitAnswer);
     return;
   }
@@ -84,25 +89,30 @@ function submitAnswer(e) {
     attemptNumber === 5 &&
     inputText !== solution
   ) {
-    answerWrongSpan.innerHTML = solution;
+    solutionSpan.innerHTML = solution;
     answerWrong.classList.add("display");
-    // btn.removeEventListener("click", submitAnswer);
     input.removeEventListener("keyup", submitAnswer);
     return;
   }
 }
 
-answerCorrect.addEventListener("click", () => {
-  answerCorrect.classList.remove("display");
-});
-answerWrong.addEventListener("click", () => {
-  answerWrong.classList.remove("display");
-});
-
 function checkValidWord(word) {
   const isOnlyLetters = /^[a-zA-Z]+$/.test(word);
   const isFiveLetters = word.length === 5;
   return isFiveLetters && isOnlyLetters;
+}
+
+function updateHtml() {
+  const htmlContent = answers
+    .map(
+      (answer) =>
+        `<div class='answer'>${answer
+          .split("")
+          .map((letter) => `<span class='letter-span'>${letter}</span>`)
+          .join("")}</div>`
+    )
+    .join("");
+  answersDiv.innerHTML = htmlContent;
 }
 
 function checkAnswers() {
@@ -129,17 +139,4 @@ function checkAnswers() {
       }
     });
   });
-}
-
-function updateHtml() {
-  const htmlContent = answers
-    .map(
-      (answer) =>
-        `<div class='answer'>${answer
-          .split("")
-          .map((letter) => `<span class='letter-span'>${letter}</span>`)
-          .join("")}</div>`
-    )
-    .join("");
-  answersDiv.innerHTML = htmlContent;
 }
